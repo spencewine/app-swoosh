2ed04b56b76870ef14503ca707e03aea6566b298
Spencer Goodwine <sdgoodwine@gmail.com>
2019-06-20 11:09:06 -0500

    finished lesson 1

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Spencer Goodwine on 6/19/19.
+//  Copyright © 2019 Spencer Goodwine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
