b4a8fe8d9c189b4c6a57da2070942462b9459ba5
Spencer Goodwine <sdgoodwine@gmail.com>
2019-06-20 17:12:27 -0500

    finished lesson 9

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Spencer Goodwine on 6/20/19.
+//  Copyright © 2019 Spencer Goodwine. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    var desiredLeague: String?
+    
+    var selectedSkillLevel:String?
+    
+    
+}
